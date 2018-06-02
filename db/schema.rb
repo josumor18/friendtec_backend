f017924a8077af2c7fff52642e742b832e1e52cb
Carlos Josué Morales R <carlosjosum@hotmail.com>
2018-06-01 18:16:22 -0600

    Add rfoto to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530161236) do
+ActiveRecord::Schema.define(version: 20180602001426) do
 
   create_table "users", force: :cascade do |t|
     t.string   "carnet"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180530161236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "carrera"
+    t.string   "avatar"
+    t.string   "rfoto"
   end
 
 end
