f0d97ca2ecff23ec1ed633e54bf28e7da1c94146
Carlos Josué Morales R <carlosjosum@hotmail.com>
2018-06-07 22:51:37 -0600

    Notifications scaffold

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -6,12 +6,27 @@ module Api
       user = User.where(id: params[:id]).first
 
       if(user)
-        notifi = []
         notif = Notification.where(id_user: user.id)
         notif.order("created_at DESC")
         render json: { status: 'SUCCESS', message: 'Notificaciones obtenidas', notificaciones: notif}, status: :ok
       else
         render json: { status: 'INVALID', message: 'Error al obtener notificaciones'}, status: :unauthorized
+      end
+    end
+
+    def set_false
+      user = User.where(id: params[:id]).first
+
+      if(user)
+        notificaciones = Notification.where(id_user: user.id)
+        notificaciones.each do |notif|
+          notif.visto = true
+          notif.save
+        end
+
+      #  render json: { status: 'SUCCESS', message: 'Notificaciones modificadas'}, status: :ok
+      #else
+      #  render json: { status: 'INVALID', message: 'Error al modificar notificaciones'}, status: :unauthorized
       end
     end
   end
